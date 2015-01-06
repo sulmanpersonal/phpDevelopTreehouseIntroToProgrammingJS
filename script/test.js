9c15f2eaf2a6a50156fd0f7e90c1fa628a5cdadb
Sulman Baig <sulmanpersonal@gmail.com>
2015-01-06 04:02:42 +0100

    chapter 02: Control Structures

--- script/test.js
+++ script/test.js
@@ -4,8 +4,28 @@
 console.log("Hello from test.js");
 console.log("Hello again!");
 
-var name = prompt("What is your name?");
+console.log("Before");
+/*if(false) {
+    console.log("If Block");
+}*/
 
-alert("Hello "+ name);
+var counter = 10;
 
-console.log("User name is "+name);
+
+
+while(counter) {
+    console.log("hello World!");
+    counter--;
+}
+
+
+
+while(prompt("What is your name?")) {
+    console.log("Got your name.");
+}
+
+for (counter=10;counter;counter--) {
+    console.log("Hello World! "+counter);
+}
+
+console.log("After");
