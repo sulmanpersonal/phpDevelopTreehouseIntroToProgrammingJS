b9f4406d501c7fab84f5274b098cc2d2dbd4baba
Sulman Baig <sulmanpersonal@gmail.com>
2015-01-06 04:32:23 +0100

    chapter 03: Objects and Arrays

--- script/test.js
+++ script/test.js
@@ -1,31 +1,25 @@
 /**
  * Created by Sulman on 06-Jan-15.
  */
-console.log("Hello from test.js");
-console.log("Hello again!");
+/*
+var friends = ["Nick","Micheal","Amit", [true, 1, null]];
+console.log(friends);
+console.log(friends.length);
+console.log(friends[3]);
 
-console.log("Before");
-/*if(false) {
-    console.log("If Block");
+for(var i=0; i<friends.length ; i+=1) {
+    console.log(friends[i]);
 }*/
 
-var counter = 10;
-
-
-
-while(counter) {
-    console.log("hello World!");
-    counter--;
+var me = {
+    first_name: "Mirza Sulman",
+    last_name: "Baig",
+    "employee number":1
 }
 
+console.log(me.first_name);
+console.log(me.last_name);
+console.log(me["employee number"]);
 
-
-while(prompt("What is your name?")) {
-    console.log("Got your name.");
-}
-
-for (counter=10;counter;counter--) {
-    console.log("Hello World! "+counter);
-}
-
-console.log("After");
+var key = "first_name";
+console.log(me[key]);
