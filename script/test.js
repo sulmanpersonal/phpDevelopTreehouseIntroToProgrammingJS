08329f82ac2d8f4fb346f04c3729bb4a88f74af9
Sulman Baig <sulmanpersonal@gmail.com>
2015-01-06 04:46:34 +0100

    chapter 04: functions

--- script/test.js
+++ script/test.js
@@ -1,25 +1,12 @@
 /**
  * Created by Sulman on 06-Jan-15.
  */
-/*
-var friends = ["Nick","Micheal","Amit", [true, 1, null]];
-console.log(friends);
-console.log(friends.length);
-console.log(friends[3]);
-
-for(var i=0; i<friends.length ; i+=1) {
-    console.log(friends[i]);
-}*/
-
-var me = {
-    first_name: "Mirza Sulman",
-    last_name: "Baig",
-    "employee number":1
+var sayHello = function () {
+    console.log("Hello World!");
+};
+sayHello();
+var doubleNumber = function(num) {
+    return num*2;
 }
 
-console.log(me.first_name);
-console.log(me.last_name);
-console.log(me["employee number"]);
-
-var key = "first_name";
-console.log(me[key]);
+console.log(doubleNumber(2));
