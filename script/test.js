dfeb41c19789af95e9e77c7241ecfd4a7e2da668
Sulman Baig <sulmanpersonal@gmail.com>
2015-01-06 03:33:13 +0100

    chapter 01: Basics

---
+++ script/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Sulman on 06-Jan-15.
+ */
+console.log("Hello from test.js");
+console.log("Hello again!");
+
+var name = prompt("What is your name?");
+
+alert("Hello "+ name);
+
+console.log("User name is "+name);
